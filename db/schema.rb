aedb23c4885ceb8bc1f7d1df1ece1b75d560423b
Khanhtn <khanhtang1403@gmail.com>
2022-11-08 16:55:21 +0700

    Sent mail

--- db/schema.rb
+++ db/schema.rb
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2022_11_07_024722) do
   end
 
   create_table "favorites", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.integer "favorited_id"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
