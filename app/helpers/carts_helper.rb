bb070f6c0243716065df4d77e1047a61255ac1e6
DD <zzvvmm96@gmail.com>
2022-11-10 21:20:55 +0700

    fix add to cart

--- app/helpers/carts_helper.rb
+++ app/helpers/carts_helper.rb
@@ -1,18 +1,23 @@
 module CartsHelper
   def current_cart
     session[:cart] ||= Array.new
+    session[:cart] = session[:cart].map(&:symbolize_keys)
+    return session[:cart]
   end
     
   def find_product_in_cart product
-    current_cart.find{|item| item["product_id"] == product.id}
+    
+    current_cart.find do |item|
+      item[:product_id] == product.id
+    end
   end
 
   def get_line_items_in_cart
     cart_items = []
     current_cart.each do |item|
-      product = Product.find_by(id: item["product_id"])
+      product = Product.find_by(id: item[:product_id])
       if product
-        cart_items << {product: product, quantity: item["quantity"]}
+        cart_items << {product: product, quantity: item[:quantity]}
       else
         current_cart.delete(item)
       end
