196626715f51d9839c89c096623bfc16773a2b54
khanhtang <115977948+khanhtang@users.noreply.github.com>
2022-11-14 10:19:02 +0700

    Revert "Revert "Update ajax""

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,23 +1,16 @@
 class FavoritesController < ApplicationController
-  def create
-    @favorite = Favorite(favorited: @product, user_id: current_user.id)
-    respond_to do |format| 
-      if @favorite.save
-        flash.now[:success] = "You have successfully added this product to your favorites list"
-        format.js { render 'favorite.js.erb' }
-      else
-        flash.now[:alert] = "Something when wrong!"
-        format.js { render 'favorite.js.erb'}
-      end
+  def update
+    favorite = Favorite.where(product: Product.find(params[:product]), user: current_user)
+    if favorite == []
+      Favorite.create(product: Product.find(params[:product]), user: current_user)
+      @favorite_exists = true
+    else
+      favorite.destroy_all
+      @favorite_exists = false
     end
-  end
-
-  def destroy
-    @favorite = Favorite.where(favorited_id: @product.id, user_id: current_user.id)
     respond_to do |format|
-      @favorite.first.destroy
-      flash[:now] = "You have successfully removed this product from your favorites list"
-      format.js { render 'favorite.js.erb'}
+      format.html
+      format.js
     end
   end
 end
